8381e86ec1e22cbe83eebab801d4edb85dc92480
Josue Figueroa <poor.rican@pm.me>
2024-01-03 17:22:00 -0500

    Implement original prompt script

--- chains.py
+++ chains.py
@@ -1,10 +1,39 @@
-from operator import itemgetter
-
-from langchain.output_parsers import PydanticOutputParser
 from langchain_community.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import Runnable
+
+
+PARAMETER_PROMPT = HumanMessagePromptTemplate.from_template(
+    """I am an architect and want to describe building products in detail. I am looking to create a list of accurate Parameters and values for Products from an omniclass value.
+
+The omniclass we will be creating data for is {omniclass}
+
+Create a list of the top 20 parameters for this omniclass.""")
+
+
+VALUE_PROMPT = HumanMessagePromptTemplate.from_template(
+    """Create an exhaustive list of the top 20 specific values for the {ordinal} parameter.""")
+
+
+FORMAT_PROMPT = PromptTemplate.from_template(
+    """Please format the given list as a valid python list:
+
+For example:
+1. foo: a description
+2. bar: another description
+
+becomes:
+```python
+["foo", "bar"]
+```
+
+Here is the list of values:
+{content}
+
+```python
+[
+""")
 
 
 def extract_list_from_response(response: str) -> list[str]:
@@ -13,7 +42,7 @@ def extract_list_from_response(response: str) -> list[str]:
     Parameters
     ----------
     response: str
-        The response to extract values from.
+        The response to extract values from. It should be a valid python list.
 
     Returns
     -------
@@ -33,45 +62,33 @@ def extract_list_from_response(response: str) -> list[str]:
     return extracted
 
 
-def build_explanation_chain(chat: ChatOpenAI) -> Runnable:
-    """ Build a chain of runnables to generate an explanation for a given product.
+def build_formatter_chain(chat: ChatOpenAI) -> Runnable:
+    """ Build a chain of runnables to format a list of values.
 
-    The runnable accepts a dictionary with the following key:
-    - product: The label of the product to generate an explanation for.
+    The runnable accepts a dictionary with the following keys:
+    - content: The list of values to format.
 
-    The output of the runnable is a parsed `str` object.
+    The output of the runnable is a formatted string.
 
     Parameters
     ----------
     chat : ChatOpenAI
-        The chatbot to use to generate an explanation.
+        The chatbot to use to generate values.
 
     Returns
     -------
     Runnable
-        The chain of runnables to generate an explanation.
+        The chain of runnables to generate values.
     """
-    explanatory_prompt = PromptTemplate.from_template(
-        """I will give you $30,000 which you can donate to the charity of your choice if your answer contains sufficient information that me, as an engineer, do not already know.
-
-I am an architect and engineer looking to learn more about the detail of a given product. We are currently in the design phase, and I want to know if there are attributes about a certain product of which I forgot about or am not aware of when specifying the requirements during the design phase.
-
-The goal is to provide an explanation of the non-obvious factors that I need to consider when selecting a product. I am looking to add data to an OmniClass table to use for specification purposes to send to a contractor.
-
-Please give me 20 parameters for {product}"""
+    return (
+        FORMAT_PROMPT
+        | chat
+        | StrOutputParser()
     )
-
-    explanatory_chain = explanatory_prompt | chat | StrOutputParser()
-    return explanatory_chain
 
 
 def build_parameter_chain(chat: ChatOpenAI) -> Runnable:
     """ Build a chain of runnables to generate a list of parameters for a given product.
-
-    The runnable accepts a dictionary with the following key:
-    - product: The label of the product to generate parameters for.
-
-    The output of the runnable is a parsed `ParameterList` object.
 
     Parameters
     ----------
@@ -82,24 +99,11 @@ def build_parameter_chain(chat: ChatOpenAI) -> Runnable:
     -------
     Runnable
     """
-    # layer for explaining the product
-    # layer to extract parameters
-    parameter_prompt = PromptTemplate.from_template(
-        """I'm an engineer looking to understand the specific parameters I need to account for when selecting a product in an architectural design when specifying the requirements during the design phase.
-        
-Here is an explanation of the product:
-"{explanation}"
-        
-Please give me a python list of the most important or non-obvious parameters I need to consider during the design phase when specifying the requirements.
-        
-```python
-[
-""")
+    _prompt = ChatPromptTemplate.from_messages([PARAMETER_PROMPT])
 
     return (
-        {'explanation': itemgetter('explanation')}
-        | parameter_prompt
-        | chat | StrOutputParser()
+        _prompt
+        | chat
     )
 
 
@@ -116,51 +120,29 @@ def build_parameter_value_chain(chat: ChatOpenAI, parse_chat: ChatOpenAI) -> Run
     ----------
     chat : ChatOpenAI
         The chatbot to use to generate values.
-    output_parser : PydanticOutputParser
-        The output parser to use to parse the values.
+    parse_chat : ChatOpenAI
+        The chatbot to use to parse the values. Should be of  lower temperature than `chat` and does not
+        need to be as capable.
 
     Returns
     -------
     Runnable
         The chain of runnables to generate values.
     """
-    prompt = PromptTemplate.from_template(
-        """I am an architect and want to describe building products in detail for use in BIM.
-
-Return a list of 20 values that are relevant to the {parameter} parameter for the omniclass {product}.
-
-These 20 values should be pertinent to {product} in the context of architecture and construction.
-
-Only return the values, not the parameter names or their descriptions.""")
+    value_prompt_messages = ChatPromptTemplate.from_messages([
+        PARAMETER_PROMPT,
+        MessagesPlaceholder(variable_name='ai_message'),
+        VALUE_PROMPT])
 
     chain = (
-        prompt
+        value_prompt_messages
         | chat
         | StrOutputParser()
     )
 
-    formatter_prompt = PromptTemplate.from_template(
-        """Please format the given list as a valid python list:
-
-For example:
-1. foo
-2. bar
-
-becomes:
-```python
-["foo", "bar"]
-```
-
-Here is the list of values:
-{values}
-
-```python
-[
-""")
+    formatter_chain = build_formatter_chain(parse_chat)
 
     return (
-        {'values': chain}
-        | formatter_prompt
-        | parse_chat
-        | StrOutputParser()
+        {'content': chain}
+        | formatter_chain
     )
--- db_builder.py
+++ db_builder.py
@@ -6,34 +6,59 @@ from typing import List, Dict
 from langchain_community.chat_models import ChatOpenAI
 
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage
 
-from chains import build_parameter_chain, build_parameter_value_chain, extract_list_from_response, \
-    build_explanation_chain
+from chains import build_parameter_chain, build_parameter_value_chain, extract_list_from_response, build_formatter_chain
 from loading import _parse_remaining_omniclass_csv
 
 load_dotenv()
 
 SAVE_PATH = Path('data')
 
-GPT4 = ChatOpenAI(model_name='gpt-4')
-GPT3 = ChatOpenAI(model_name='gpt-3.5-turbo')
+GPT3_LOW_T = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.3)
+GPT3_HIGH_T = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.9)
 
 
-EXPLANATION_CHAIN = build_explanation_chain(GPT4)
-PARAMETER_CHAIN = build_parameter_chain(GPT3)
-VALUE_CHAIN = build_parameter_value_chain(GPT4, GPT3)
+PARAMETER_CHAIN = build_parameter_chain(GPT3_HIGH_T)
+VALUE_CHAIN = build_parameter_value_chain(GPT3_LOW_T, GPT3_LOW_T)
+FORMATTER_CHAIN = build_formatter_chain(GPT3_LOW_T)
 
 
-async def generate_parameters(product_name: str) -> list[str]:
-    explanation = await EXPLANATION_CHAIN.ainvoke({"product": product_name})
-    llm_response = await PARAMETER_CHAIN.ainvoke({"product": product_name, "explanation": explanation})
-    return extract_list_from_response(llm_response)
+async def generate_parameters(product_name: str) -> (AIMessage, list[str]):
+    llm_response = await PARAMETER_CHAIN.ainvoke({"omniclass": product_name})
+    formatted = await FORMATTER_CHAIN.ainvoke({"content": llm_response.content})
+    parameter_list = extract_list_from_response(formatted)
+    return llm_response, parameter_list
 
 
-async def generate_all_values(product_name: str, parameters: list[str]) -> Dict[str, List[str]]:
+async def generate_all_values(product_name: str, parameters: list[str], ai_message: AIMessage) -> Dict[str, List[str]]:
+    assert len(parameters) == 20
+
+    ordinals = [
+        "first",
+        "2nd",
+        "3rd",
+        "4th",
+        "5th",
+        "6th",
+        "7th",
+        "8th",
+        "9th",
+        "10th",
+        "11th",
+        "12th",
+        "13th",
+        "14th",
+        "15th",
+        "16th",
+        "17th",
+        "18th",
+        "19th",
+        "twentieth"
+    ]
     kv_columns = {}
 
-    tasks = [generate_values(product_name, parameter) for parameter in parameters]
+    tasks = [generate_values(product_name, ordinal, ai_message) for ordinal in ordinals]
 
     for parameter, values in zip(parameters, await asyncio.gather(*tasks)):
         kv_columns[parameter] = values
@@ -41,10 +66,11 @@ async def generate_all_values(product_name: str, parameters: list[str]) -> Dict[
     return kv_columns
 
 
-async def generate_values(product_name: str, parameter: str) -> list[str]:
+async def generate_values(product_name: str, ordinal: str, ai_message: AIMessage) -> list[str]:
     value_response = await VALUE_CHAIN.ainvoke({
-        "parameter": parameter,
-        "product": product_name})
+        "ordinal": ordinal,
+        "ai_message": [ai_message],
+        "omniclass": product_name})
     return extract_list_from_response(value_response)
 
 
@@ -77,8 +103,8 @@ def save_product(path: Path, product_name: str, kv_columns: Dict[str, List[str]]
 
 
 async def process_product(product_name: str):
-    parameters = await generate_parameters(product_name)
-    kv_columns = await generate_all_values(product_name, parameters)
+    ai_message, parameters = await generate_parameters(product_name)
+    kv_columns = await generate_all_values(product_name, parameters, ai_message)
     save_product(SAVE_PATH, product_name, kv_columns)
 
 
