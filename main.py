96198c2760ac17ea42098bf87e9b897960b35395
Josue Figueroa <poor.rican@pm.me>
2024-01-04 10:47:43 -0500

    Prepare main.py for user execution

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 from asyncio import sleep, gather, run
 from pathlib import Path
 
-from src.db_builder import process_product
+from src.db_builder import process_product, SAVE_PATH
 from src.loading import OmniClass, parse_remaining
 
 CHUNK_SIZE = 3
-REMAINING_FN = Path('remaining_omniclass.csv')
+REMAINING_FN = Path('remaining.csv')
 
 
 async def run_all(omniclasses: list[OmniClass]):
@@ -22,9 +24,31 @@ async def run_all(omniclasses: list[OmniClass]):
         tasks = [process_product(product_name) for product_name in chunk]
         await gather(*tasks)
 
+    # print the number of products that were processed
+    print(f"\nProcessed {len(omniclasses)} products.")
+    print("Done!")
+    exit(0)
+
+
+def create_data_directory():
+    """ Create the data directory if it does not exist. """
+    if not SAVE_PATH.is_dir():
+        SAVE_PATH.mkdir()
+        print(f"Created directory: {SAVE_PATH}")
+
 
 if __name__ == '__main__':
+    # clear terminal window
+    print('\033c')
 
-    OMNICLASS_LIST = parse_remaining(REMAINING_FN)
+    # create the data directory if it does not exist
+    create_data_directory()
 
-    run(run_all(OMNICLASS_LIST))
+    # parse the remaining products
+    try:
+        OMNICLASS_LIST = parse_remaining(REMAINING_FN)
+
+        run(run_all(OMNICLASS_LIST))
+    except FileNotFoundError:
+        print(f"Could not find file: {REMAINING_FN}")
+        exit(1)
