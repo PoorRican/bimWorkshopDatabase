d5e9b5c25037f677a66261451ca8fe7be71a9028
Josue Figueroa <poor.rican@pm.me>
2024-01-06 19:37:41 -0500

    Remove relative imports from "db_builder"

--- src/db_builder.py
+++ src/db_builder.py
@@ -10,8 +10,8 @@ from dotenv import load_dotenv
 from langchain_core.messages import AIMessage
 from openai import RateLimitError
 
-from .chains import build_parameter_chain, build_parameter_value_chain, extract_list_from_response, build_formatter_chain
-from .loading import OmniClass
+from chains import build_parameter_chain, build_parameter_value_chain, extract_list_from_response, build_formatter_chain
+from loading import OmniClass
 
 load_dotenv()
 
