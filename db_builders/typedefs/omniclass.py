46f03d66e7114e6789dc85860b3ae28e05b6143a
Josue Figueroa <poor.rican@pm.me>
2024-03-09 01:43:20 -0500

    Add identifier functionality to `Omniclass`

--- db_builders/typedefs/omniclass.py
+++ db_builders/typedefs/omniclass.py
@@ -1,3 +1,5 @@
+import re
+
 from pydantic import BaseModel
 
 
@@ -11,3 +13,28 @@ class Omniclass(BaseModel):
 
     def __str__(self):
         return f"{self.number} {self.name}"
+
+    @property
+    def identifier(self) -> str:
+        """ Return the identifier for the omniclass.
+
+        This is used to generate the filename for the CSV file.
+        """
+        return str(self)
+
+    @staticmethod
+    def parse_identifier(identifier: str) -> tuple[str, str]:
+        """ Parse an Omniclass identifier into the omniclass number and name.
+
+        Parameters:
+            identifier (str): The identifier to parse.
+
+        Returns:
+            A tuple of the omniclass number and name.
+        """
+        regex_pattern = r"(?P<number>(\d{2}-\d{2}(\s\d{2})*)+) (?P<name>.*)"
+        match = re.match(regex_pattern, identifier)
+        if not match:
+            raise ValueError(f"Could not parse filename: {identifier}")
+        else:
+            return match.group('number'), match.group('name')
